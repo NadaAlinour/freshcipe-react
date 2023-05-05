8eff6141daad441a669dca56438e75513b52a192
Bassant <126110020+BassantMAbbas@users.noreply.github.com>
2023-05-05 23:43:41 +0300

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,33 @@
 export default function Signup() {
     return(
         <>
-            <h1>this is signup</h1>
+            <div className="sign-up-form">
+            <h2>Sign Up</h2>
+            <form>
+                <h3>Create your account</h3>
+                <div class="group-input">
+                    <i class="user"></i>
+                    <p>User Name:</p>
+                    <input type="text" placeholder="Name"/>
+                </div>
+                <div class="group-input">
+                    <i class="envelope"></i>
+                    <p>Email:</p>
+                    <input type="Email" placeholder="Email"/>
+                </div>
+                <div class="group-input">
+                    <i class="Lock"></i>
+                    <p>Password:</p>
+                    <input type="Password" placeholder="Password"/>
+                </div>
+                <div class="group-input">
+                    <i class="Lock"></i>
+                    <p>Confirm Password:</p>
+                    <input type="Password" placeholder="Password"/>
+                </div>
+                <button type="submit">Sign Up</button>
+            </form>
+           </div>
         </>
     )
 }
