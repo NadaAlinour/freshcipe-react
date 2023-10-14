5d82d00234f279d040bd914f5d6aced66da25c03
Nada <lime.x.na@gmail.com>
2023-10-14 20:08:18 +0300

    Add product filter layout

--- src/components/ProductFilter.jsx
+++ src/components/ProductFilter.jsx
@@ -1,7 +1,93 @@
+import "boxicons";
+import { useState } from "react";
+
 export default function ProductFilter() {
-    return (
-        <div>
-            <h4>this is product filter</h4>
+  const [isStoreHidden, setIsStoreHidden] = useState(false);
+  const [isCategoryHidden, setIsCategoryHidden] = useState(false);
+
+  return (
+    <div className="product-filter-contents">
+      <h2>Filter by</h2>
+
+      <div className="filter-block-container">
+        <div className="filter-block-header">
+          <h3>Store</h3>
+          <div
+            className="filter-header-chevron"
+            onClick={() => setIsStoreHidden(!isStoreHidden)}
+          >
+            {isStoreHidden ? (
+              <box-icon name="chevron-down" />
+            ) : (
+              <box-icon name="chevron-up" />
+            )}
+          </div>
         </div>
-    )
+        <ul className={isStoreHidden ? "hidden" : ""}>
+          <li>
+            <input type="checkbox" />
+            <p>market 1</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <p>market 2</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <p>market 3</p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="filter-block-container">
+        <div className="filter-block-header">
+          <h3>Category</h3>
+          <div
+            className="filter-header-chevron"
+            onClick={() => setIsCategoryHidden(!isCategoryHidden)}
+          >
+            {isCategoryHidden ? (
+              <box-icon name="chevron-down" />
+            ) : (
+              <box-icon name="chevron-up" />
+            )}
+          </div>
+        </div>
+        <ul className={isCategoryHidden ? "hidden" : ""}>
+          <li>
+            <input type="checkbox" />
+            <p>Fruits and vegetables (25)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <p>Meat, chicken, and fish (15)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <p>Baked goods and pastries (12)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <p>Drinks, coffee and tea (5)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <p>Snacks (21)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <p>Spices, oil and sauces (18)</p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="filter-block-container">
+        <div className="filter-block-header">
+          <h3>Price</h3>
+        </div>
+
+        <p>this is slider</p>
+      </div>
+    </div>
+  );
 }
