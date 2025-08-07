776609c7644c262f7031b50cce364e97c3e7843b
Nada <lime.x.na@gmail.com>
2025-08-07 21:17:16 +0300

    Remove strapi server directory

--- strapi-server/src/api/favourite/services/favourite.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * favourite service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::favourite.favourite');
